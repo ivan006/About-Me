9ff69bcb33987313e6098c69d101daaf106d80c0
ivan006 <ivan.copeland2015@gmail.com>
2020-03-25 22:15:37 +0200

    1

--- booking-dashboard-template/The Book360 System Dashboard_files/flash-effect.js
+++
@@ -1,8 +0,0 @@
-// flash effect start
-$(document).ready(function(){
-  $("#take-screenshot").click(function(){
-    $("body").toggleClass("flash-effect");
-    setTimeout(function(){$("body").toggleClass("flash-effect"); }, 3000);
-  });
-});
-// flash effect end
